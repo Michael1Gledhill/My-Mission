42a41a3d035f6c2e75ebe8eea93641744c311b00
topplanecreator <michael1gledhill@gmail.com>
2026-04-01 19:12:40 -0400

    2.5

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite';
-export default defineConfig({
-    base: './',
-    build: {
-        outDir: 'dist',
-        sourcemap: true
-    }
+import { defineConfig, loadEnv } from 'vite';
+export default defineConfig(function (_a) {
+    var _b;
+    var mode = _a.mode;
+    var env = loadEnv(mode, '.', '');
+    var repoName = (_b = env.GITHUB_REPOSITORY) === null || _b === void 0 ? void 0 : _b.split('/')[1];
+    var isGithubActions = env.GITHUB_ACTIONS === 'true';
+    return {
+        base: isGithubActions && repoName ? "/".concat(repoName, "/") : '/',
+        build: {
+            outDir: 'dist',
+            sourcemap: true
+        }
+    };
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  base: './',
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+  const repoName = env.GITHUB_REPOSITORY?.split('/')[1];
+  const isGithubActions = env.GITHUB_ACTIONS === 'true';
+
+  return {
+    base: isGithubActions && repoName ? `/${repoName}/` : '/',
+    build: {
+      outDir: 'dist',
+      sourcemap: true
+    }
+  };
 });
